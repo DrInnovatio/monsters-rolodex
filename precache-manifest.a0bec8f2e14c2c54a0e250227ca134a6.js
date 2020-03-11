3bcea8636234e75a99e75df876427ca8ab4f45c0
DrInnovatio <turing1723@gmail.com>
2020-03-11 17:49:41 +1000

    Updates

---
+++ precache-manifest.a0bec8f2e14c2c54a0e250227ca134a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7318d03d50b56015c0f985f9b4b8e562",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eb81df4106eb11361c87",
+    "url": "/monsters-rolodex/static/css/main.2a35f750.chunk.css"
+  },
+  {
+    "revision": "df8d593596a3ed3d89ae",
+    "url": "/monsters-rolodex/static/js/2.8bf845fa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.8bf845fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb81df4106eb11361c87",
+    "url": "/monsters-rolodex/static/js/main.e8bc1c03.chunk.js"
+  },
+  {
+    "revision": "3ce411994159d536241c",
+    "url": "/monsters-rolodex/static/js/runtime-main.eddd01e4.js"
+  }
+]);
